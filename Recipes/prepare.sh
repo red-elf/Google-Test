4ae7b352df7aabb7f8180a6afbbe55c547030e80
Milos Vasic <i@mvasic.ru>
2022-10-20 15:49:48 +0300

    The 'prepare' missing recipe.

--- Recipes/prepare.sh
+++ Recipes/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "To be implemented!"
-exit 1
+echo "Preparing the Google Test for the installation" && \
+  git submodule init && git submodule update && \
+  echo "The Google Test is prepared for the installation"
